1d11cd618094338b93527e97ef5fc09579d67066
Brendan Hytrek <bjhytrek@gmail.com>
2015-03-02 11:35:45 -0500

    working on the styling

--- test/index.php
+++ test/index.php
@@ -7,11 +7,12 @@
         <?php include 'modules/header.php'; ?>
     <body>
         <?php $active_page
-                = 0; ?>
+                = 0;
+        ?>
 <?php include 'modules/navbar_top.php'; ?>
         <!-- Responsive Grid System.-->
-        <div class="container" id="main_container">
 
+        <div class="container">
             <!-- Carousel image slider -->
             <div id="carousel" class="carousel slide" data-ride="carousel">
                 <!-- Indicators -->
@@ -54,46 +55,54 @@
                     <span class="glyphicon glyphicon-chevron-right"></span>
                 </a>
             </div>
+        </div>
+
+        <div class="container" id="main_container">
             <!-- Main content-->
             <div class="jumbotron">
                 <p>Premier Murphy Beds manufactures the highest quality beds in the business with the best prices! We use only the best materials, and build our beds right here in the USA. Our current line features top-grade wood with beautiful options for any room in the home. Our space saving features create a beautifully furnished room that maximizes space and efficiency.*mattress not included*</p>
             </div>
-            <!--Queen thumbnail-->
-            <div class="row ">
-                <div class="col-xs-4 col-md-4">
-
-                    <div class="thumbnail">
-                        <a href="queen_beds.php"><img src="media/images/beds/queen_metro.jpg" alt="Queen Sized Murhpy Bed"></a>
-                        <div class="caption">
-                            <h3>Queen Sized Murphy Beds</h3>
-                            
-                        </div>
-                    </div>
+            <!--Queen bed-->
+            <div class="row">
+                <div class="col-md-3 col-xs-4 col-lg-3 col-sm-3">
+                    
+                        <a href="queen_beds.php"><img src="media/images/beds/new_queen.jpg" alt="Queen Sized Murhpy Bed"></a>   
+                
                 </div>
-                <div class="col-xs-4 col-md-4">
-
-                    <div class="thumbnail">
-                        <a href="full_beds.php"><img src="media/images/beds/queen_half.jpg" alt="Full Sized Murhpy Bed"></a>
-                        <div class="caption">
-                            <h3>Full Sized Murphy Beds</h3>
-                            
-                        </div>
-                    </div>
+                <div class="col-lg-6 col-sm-6 col-xs-6 col-md-6">
+                    <a href="queen_beds.php"><h3>Queen Sized Murphy Bed</h3></a>
+                    <p>Our queen murphy bed will do really funny things to your back.</p>
                 </div>
-                <div class="col-xs-4 col-md-4">
-
-                    <div class="thumbnail">
-                        <a href="twin_beds.php"><img src="media/images/beds/queen_1.jpg" alt="Double Sized Murhpy Bed"></a>
-                        <div class="caption">
-                            <h3>Twin Sized Murphy Beds</h3>
-                            
-                        </div>
-                    </div>
-                </div>
-
-
             </div>
-            <?php include 'modules/warranty_return_policy.php'; ?>
+            <hr>
+            
+            <!--Full bed-->
+            <div class="row">
+                <div class="col-md-3 col-xs-4 col-lg-3 col-sm-3">
+                    
+                        <a href="full_beds.php"><img src="media/images/beds/new_full.jpg" alt="Full Sized Murhpy Bed"></a>   
+                
+                </div>
+                <div class="col-lg-6 col-sm-6 col-xs-6 col-md-6">
+                    <a href="full_beds.php"><h3>Full Sized Murphy Bed</h3></a>
+                    <p>Our Full murphy bed will do really funny things to your back.</p>
+                </div>
+            </div>
+            <hr>
+            <!--Twin Bed -->
+            <div class="row">
+                <div class="col-md-3 col-xs-4 col-lg-3 col-sm-3">
+                    
+                        <a href="twin_beds.php"><img src="media/images/beds/new_twin.jpg" alt="Twin Sized Murhpy Bed"></a>   
+                
+                </div>
+                <div class="col-lg-6 col-sm-6 col-xs-6 col-md-6">
+                    <a href="twin_beds.php"><h3>Twin Sized Murphy Bed</h3></a>
+                    <p>Our Twin murphy bed will do really funny things to your back.</p>
+                </div>
+            </div>
+    
+<?php include 'modules/warranty_return_policy.php'; ?>
         </div>
 
 
