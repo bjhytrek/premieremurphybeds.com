150a1535730052c8a99a774a8f138ae4dc6b2db1
Brendan Hytrek <bjhytrek@gmail.com>
2015-07-07 16:25:04 -0600

    Adding google analytics
    adding google analytic tracking.

--- pmb.com_no_cart/modules/footer.php
+++ pmb.com_no_cart/modules/footer.php
@@ -8,6 +8,6 @@
 					</div>
 				</div>
 				
-		
+		<?php include_once("analyticstracking.php") ?>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
